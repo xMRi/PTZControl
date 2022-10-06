221b560aa468f52206ce4d7de465f31d0946217a
Martin Richter <xrichter@gmx.de>
2022-10-06 16:13:43 +0200

    Added Accelerator to switch between the cameras.

--- PTZControl/resource.h
+++ PTZControl/resource.h
@@ -16,42 +16,42 @@
 #define IDC_CHECK1                      1001
 #define IDC_CH_LOGITECHCONTROL          1001
 #define IDC_BT_UP                       1002
-#define IDC_ED_TOOLTIP_1_1                1002
+#define IDC_ED_TOOLTIP_1_1              1002
 #define IDC_BT_DOWN                     1003
-#define IDC_ED_TOOLTIP_1_2                1003
+#define IDC_ED_TOOLTIP_1_2              1003
 #define IDC_BT_HOME                     1004
-#define IDC_ED_TOOLTIP_1_3                1004
+#define IDC_ED_TOOLTIP_1_3              1004
 #define IDC_BT_MEMORY                   1005
-#define IDC_ED_TOOLTIP_1_4                1005
+#define IDC_ED_TOOLTIP_1_4              1005
 #define IDC_BT_PRESET1                  1006
-#define IDC_ED_TOOLTIP_1_5                1006
+#define IDC_ED_TOOLTIP_1_5              1006
 #define IDC_BT_PRESET2                  1007
-#define IDC_ED_TOOLTIP_1_6                1007
+#define IDC_ED_TOOLTIP_1_6              1007
 #define IDC_BT_PRESET3                  1008
-#define IDC_ED_TOOLTIP_1_7                1008
+#define IDC_ED_TOOLTIP_1_7              1008
 #define IDC_BT_PRESET4                  1009
-#define IDC_ED_TOOLTIP_1_8                1009
+#define IDC_ED_TOOLTIP_1_8              1009
 #define IDC_BT_PRESET5                  1010
 #define IDC_ED_CAMERA                   1010
 #define IDC_BT_PRESET6                  1011
 #define IDC_EDIT1                       1011
 #define IDC_ED_MOTORTIME                1011
 #define IDC_BT_PRESET7                  1012
-#define IDC_ED_TOOLTIP_2_1                1012
+#define IDC_ED_TOOLTIP_2_1              1012
 #define IDC_BT_PRESET8                  1013
 #define IDC_ED_TOOLTIP_2_2              1013
 #define IDC_BT_ZOOM_IN                  1014
-#define IDC_ED_TOOLTIP_2_3               1014
+#define IDC_ED_TOOLTIP_2_3              1014
 #define IDC_BT_ZOOM_OUT                 1015
-#define IDC_ED_TOOLTIP_2_4               1015
+#define IDC_ED_TOOLTIP_2_4              1015
 #define IDC_BT_EXIT                     1016
-#define IDC_ED_TOOLTIP_2_5               1016
+#define IDC_ED_TOOLTIP_2_5              1016
 #define IDC_BT_SETTINGS                 1017
-#define IDC_ED_TOOLTIP_2_6               1017
+#define IDC_ED_TOOLTIP_2_6              1017
 #define IDC_BT_WEBCAM1                  1018
-#define IDC_ED_TOOLTIP_2_7               1018
+#define IDC_ED_TOOLTIP_2_7              1018
 #define IDC_BT_WEBCAM2                  1019
-#define IDC_ED_TOOLTIP_2_8               1019
+#define IDC_ED_TOOLTIP_2_8              1019
 #define DC_BT_SETTINGS                  32791
 
 // Next default values for new objects
@@ -59,7 +59,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        135
-#define _APS_NEXT_COMMAND_VALUE         32793
+#define _APS_NEXT_COMMAND_VALUE         32799
 #define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
